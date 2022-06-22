6fcf8a6497d6ccb7a1b6d88bb63bd648c7b6b708
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-06-22 14:44:18 +0530

    Update admin.py

--- plugins/database/admin.py
+++ plugins/database/admin.py
@@ -11,30 +11,8 @@ from plugins.functions.display_progress import humanbytes
 from plugins.database.bcast import broadcast_handler
 
 
-@Client.on_message(filters.command("status") & filters.user(Config.OWNER_ID) & ~filters.edited)
-async def status_handler(_, m: Message):
-    total, used, free = shutil.disk_usage(".")
-    total = humanbytes(total)
-    used = humanbytes(used)
-    free = humanbytes(free)
-    cpu_usage = psutil.cpu_percent()
-    ram_usage = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    total_users = await db.total_users_count()
-    await m.reply_text(
-        text=f"**Total Disk Space:** {total} \n"
-             f"**Used Space:** {used}({disk_usage}%) \n"
-             f"**Free Space:** {free} \n"
-             f"**CPU Usage:** {cpu_usage}% \n"
-             f"**RAM Usage:** {ram_usage}%\n\n"
-             f"**Total Users in DB:** `{total_users}`",
-        parse_mode="Markdown",
-        quote=True
-    )
 
 
-@Client.on_message(filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply & ~filters.edited)
+@Client.on_message(filters.command("broadcast") & filters.user(Config.OWNER_ID) & filters.reply)
 async def broadcast_in(_, m: Message):
     await broadcast_handler(m)
-
-
